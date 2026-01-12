113829dccea5c8da812c97434c35ed69be718101
dusengepeggy <dickinepeggy100@gmail.com>
2026-01-12 10:43:32 +0200

    ft: postgres sql queries

---
+++ SQL-queries/smart_ecommerce_schema.sql
@@ -0,0 +1,64 @@
+BEGIN;
+CREATE TYPE user_role AS ENUM ('Admin', 'Customer');
+CREATE TYPE order_status AS ENUM ('Pending', 'Shipped', 'Delivered', 'Cancelled');
+CREATE TABLE Category (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT
+);
+CREATE TABLE "User" (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role user_role NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Product (
+    product_id SERIAL PRIMARY KEY,
+    category_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
+    created_at DATE DEFAULT CURRENT_DATE,
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES Category(category_id) ON DELETE CASCADE
+);
+CREATE TABLE Inventory (
+    inventory_id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL UNIQUE,
+    stock_quantity INT NOT NULL DEFAULT 0 CHECK (stock_quantity >= 0),
+    warehouse_location VARCHAR(255),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_product_inventory FOREIGN KEY (product_id) REFERENCES Product(product_id) ON DELETE CASCADE
+);
+
+CREATE TABLE "Order" (
+    order_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_date DATE DEFAULT CURRENT_DATE,
+    total_amount DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    status order_status DEFAULT 'Pending',
+    CONSTRAINT fk_user_order FOREIGN KEY (user_id) REFERENCES "User"(user_id) ON DELETE RESTRICT
+);
+
+CREATE TABLE OrderItem (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    unit_price DECIMAL(10, 2) NOT NULL,
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES "Order"(order_id) ON DELETE CASCADE,
+    CONSTRAINT fk_product_orderItem FOREIGN KEY (product_id) REFERENCES Product(product_id) ON DELETE RESTRICT
+);
+CREATE TABLE Review (
+    review_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    review_date DATE DEFAULT CURRENT_DATE,
+    CONSTRAINT fk_user_review FOREIGN KEY (user_id) REFERENCES "User"(user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_product_review FOREIGN KEY (product_id) REFERENCES Product(product_id) ON DELETE CASCADE
+);
+COMMIT;
